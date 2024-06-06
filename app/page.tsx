8b9cac8190f8d4235d611ee57873fc60b41317b0
Abbas-Askari <abbasaskari797@gmail.com>
2024-06-06 17:31:48 +0500

    problem section in progress.

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,8 @@ export default function Home() {
         <HeroText />
         <SearchBar />
       </section>
-      <ScrollText text="Our mission is to empower cancer patients by providing clear, comprehensive information about the treatment options available for their specific diagnosis. We support patients and their families by offering up-to-date resources, expert insights, and personalized guidance. Whether newly diagnosed or exploring new treatments, our platform helps you make informed decisions and navigate your path to recovery with confidence and hope." />
+      <ScrollText text="Our mission is to empower cancer patients by providing clear, comprehensive information about the treatment options available for their specific diagnosis." />
+      {/* <section className="h-screen"></section> */}
     </div>
   );
 }
