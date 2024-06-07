bf030aa41898facd9b56c76fc2ad6de384effcbb
Abbas-Askari <abbasaskari797@gmail.com>
2024-06-07 11:07:50 +0500

    News section in progress

--- components/custom/news.tsx
+++ components/custom/news.tsx
@@ -19,19 +19,25 @@ function News() {
         {
             title: 'India’s newly galvanized opposition strikes back with ‘mandate to save democracy’',
             date: new Date('2022-01-03')
-        }
+        },
+        {
+            title: 'After eight months of war, here’s what Gaza’s humanitarian crisis looks like',
+            date: new Date('2022-01-01')
+        },
+        {
+            title: 'USA pulls off shock defeat of Pakistan at Men’s T20 Cricket World Cup',
+            date: new Date('2022-01-02')
+        },
+        {
+            title: 'India’s newly galvanized opposition strikes back with ‘mandate to save democracy’',
+            date: new Date('2022-01-03')
+        },
     ]
-    
-   return (
-        <section className=''>
-            <div className='flex justify-between'>
-                <h1 className='text-2xl font-bold'>News</h1>
-                <div className='flex gap-2'>
-                    <button>Latest</button>
-                    <button>Popular</button>
-                </div>
-            </div>
-            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+
+    return (
+        <section className='py-16 mb-96 bg-neutral-300 '>
+            <h1 className='text-8xl font-bold'>News</h1>
+            <div className=' p-16 px-64 flex gap-16 overflow-scroll'>
                 {news.map((n, i) => <NewsCard key={i} news={n} />)}
             </div>
         </section>
@@ -41,15 +47,21 @@ function News() {
 function NewsCard({ news }: { news: News }) {
     return (
         <div className=''>
-            <div className=''>
-                <img src={news.image} alt={news.title} />
+            <div className=' rounded-xl overflow-hidden h-[450px] w-[300px]'>
+                {
+                    news.image ?
+                    <img src={news.image} alt='img  ' />:
+                    <div className='bg-neutral-700 w-full h-full'></div>
+                }
             </div>
-            <div className=''>
-                <h2>{news.title}</h2>
-                <p>{news.date.toDateString()}</p>
+            <div className='p-4'>
+                <h2 className='text-xl'>{news.title}</h2>
+                <p className=''>{news.date.toDateString()}</p>
             </div>
         </div>
     )
 }
+
+
 
 export default News
