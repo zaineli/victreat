05fb3e3d02472ec70ea0e37ad9915c0997fd17c9
Abbas-Askari <abbasaskari797@gmail.com>
2024-06-27 16:08:08 +0500

    started trials component

---
+++ components/custom/trials.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+
+type Data = {
+    year: number;
+    cancer: number;
+};
+
+const dataArray: Data[] = [
+    { year: 1990, cancer: 33 },
+    { year: 1991, cancer: 47 },
+    { year: 1992, cancer: 55 },
+    { year: 1993, cancer: 110 },
+    { year: 1994, cancer: 123 },
+    { year: 1995, cancer: 201 },
+    { year: 1996, cancer: 316 },
+    { year: 1997, cancer: 416 },
+    { year: 1998, cancer: 635 },
+    { year: 1999, cancer: 806 },
+    { year: 2000, cancer: 1006 },
+    { year: 2001, cancer: 1047 },
+    { year: 2002, cancer: 1298 },
+    { year: 2003, cancer: 1608 },
+    { year: 2004, cancer: 2025 },
+    { year: 2005, cancer: 2372 },
+    { year: 2006, cancer: 2808 },
+    { year: 2007, cancer: 3090 },
+    { year: 2008, cancer: 3318 },
+    { year: 2009, cancer: 3536 },
+    { year: 2010, cancer: 3606 },
+    { year: 2011, cancer: 3772 },
+    { year: 2012, cancer: 3876 },
+    { year: 2013, cancer: 3917 },
+    { year: 2014, cancer: 4417 },
+    { year: 2015, cancer: 4876 },
+    { year: 2016, cancer: 5328 },
+    { year: 2017, cancer: 5630 },
+    { year: 2018, cancer: 5983 },
+    { year: 2019, cancer: 6214 },
+    { year: 2020, cancer: 6439 },
+    { year: 2021, cancer: 7427 },
+    { year: 2022, cancer: 7117 },
+    { year: 2023, cancer: 7155 },
+    { year: 2024, cancer: 4958 }
+];
+
+function Trials() {
+  return (
+    <div>Trials</div>
+  )
+}
+
+export default Trials
