b386c39d7b38deeec886352c0609d27337abcb05
Abbas-Askari <abbasaskari797@gmail.com>
2024-06-29 14:09:17 +0500

    synced wheel and graph

--- components/custom/mutationSections.tsx
+++ components/custom/mutationSections.tsx
@@ -4,7 +4,8 @@ import React, { useEffect, useRef, useState } from 'react'
 import WheelCarousel from './wheelCarousel'
 import { AnimatedBeamMultipleOutputDemo } from '../AnimatedBeam'
 import useMouseHover from '@/lib/useMouseHover';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
+import { a } from 'react-spring';
 
 
 
@@ -12,16 +13,25 @@ function MutationSection() {
     const interval = useRef<NodeJS.Timeout | null>(null);
     const ref = useRef<HTMLDivElement>(null);
     const isHovered = useMouseHover(ref);
-    const [index, setIndex] = useState(0);
-    const max = 3;
 
+    const array = [
+        <AnimatedBeamMultipleOutputDemo />,
+        <AnimatedBeamMultipleOutputDemo />,
+        <AnimatedBeamMultipleOutputDemo />,
+        <AnimatedBeamMultipleOutputDemo />,
+        <AnimatedBeamMultipleOutputDemo />,
+        <AnimatedBeamMultipleOutputDemo />,
+        <AnimatedBeamMultipleOutputDemo />,
+    ]
+
+    const [index, setIndex] = useState(0);
     useEffect(() => {
         if (interval.current) {
             clearInterval(interval.current)
         }
         if (!isHovered) {
             interval.current = setInterval(() => {
-                setIndex((prev) => (prev + 1) % max)
+                setIndex((prev) => (prev + 1) % array.length)
             }, 1000)
         }
         return () => {
@@ -31,11 +41,30 @@ function MutationSection() {
         }
     }, [])
 
+    const next = (index + 1) % array.length;
+
+    console.log({ next, index })
+
     return (
-        <div ref={ref} className='flex gap-16 p-16 rounded-2xl bg-neutral-500  text-white'>
+        <div ref={ref} className='flex gap-16 p-16 rounded-2xl bg-neutral-500 overflow-hidden  text-white'>
             <WheelCarousel className=' w-1/3' />
-            <div className=" w-2/3 flex flex-col overflow-hidden">
-                <AnimatedBeamMultipleOutputDemo />
+            <div className=" w-2/3 flex flex-col relative overflow-hidden">
+                <AnimatePresence mode='popLayout'>
+                    <motion.div
+                        key={index}
+                        initial={{
+                            transform: 'translateY(100%)'
+                        }}
+                        animate={{
+                            transform: 'translateY(0%)',
+                        }}
+                        exit={{
+                            transform: 'translateY(-100%)',
+                        }}
+                        className=" ">
+                        {array[index]}
+                    </motion.div>
+                </AnimatePresence>
             </div>
         </div>
     )
