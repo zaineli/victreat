4bd0d8fae39b88b426d268b0df07b47d3d9f5f55
Abbas-Askari <abbasaskari797@gmail.com>
2024-07-01 12:36:06 +0500

    finalized lg breakpoint on wheel carousle

--- components/custom/wheelCarousel.tsx
+++ components/custom/wheelCarousel.tsx
@@ -53,16 +53,16 @@ function WheelCarousel({ className }: WheelCarouselProps) {
         slice = slice.concat(data.slice(0, remaining))
     }
     return (
-        <div ref={ref} className={cn('flex lg:flex-col bg-red-600 justify-center', className)}>
+        <div ref={ref} className={cn('flex  lg:flex-col justify-center', className)}>
             {slice.map((item, i) => (
                 <motion.div
-                    className=' lg:origin-left origin-top  text-center  text-3xl flex-1 flex  items-center justify-center lg:text-[3.24vw] font-bold pl-2 w-max min-w-max'
+                    className=' lg:origin-left origin-top  text-center min-w-1/3 w-1/3 max-w-1/3  text-3xl flex-1 flex  items-center justify-center lg:text-[3.24vw] font-bold  '
                     initial={{
-                        transform: `translate(${large ? "0" : "-100"}%, ${!large ? "0" : "100"}%) scale(${1 - Math.abs(i - 2 + (large ? 1 : -1)) / 2})`,
+                        transform: `translate(${large ? "0" : "-100"}%, ${!large ? "0" : "100"}%) `,
                         opacity: 1 - Math.abs(i - 2 + (large ? 1 : -1)) / 2
                     }}
                     animate={{
-                        transform: `translate(0%, 0%) scale(${1 - Math.abs(i - 2) / 2})`,
+                        transform: `translate(0%, 0%) `,
                         opacity: 1 - Math.abs(i - 2) / 2,
                         transition: { duration: 0.5, ease: 'easeInOut', }
                     }}
