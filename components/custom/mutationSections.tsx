cfa27e30fa0eba4e681855e5836e40cfd82b42f5
zaineli <zali.bscs22seecs@seecs.edu.pk>
2024-07-01 15:43:08 +0500

    moving state up

--- components/custom/mutationSections.tsx
+++ components/custom/mutationSections.tsx
@@ -5,199 +5,86 @@ import WheelCarousel from "@/components/custom/wheelCarousel";
 import { AnimatedBeamMultipleOutputDemo } from "../AnimatedBeam";
 import useMouseHover from "@/lib/useMouseHover";
 import { AnimatePresence, motion } from "framer-motion";
+import { useMediaQuery } from "@/lib/useMediaQuery";
 
 function MutationSection() {
-  const interval = useRef<NodeJS.Timeout | null>(null);
-  const ref = useRef<HTMLDivElement>(null);
-  const isHovered = useMouseHover(ref);
-  const cancers = [
-    {
-      name: "Lung Cancer",
-      dataPoints: [
-        "PDL1",
-        "EGFR",
-        "KRAS",
-        "ALK",
-        "ERBB2",
-        "BRAF",
-        "PIK3CA",
-        "MET",
-      ],
-    },
-    {
-      name: "Breast Cancer",
-      dataPoints: [
-        "BRCA1",
-        "BRCA2",
-        "HER2",
-        "PIK3CA",
-        "TP53",
-        "ESR1",
-        "AKT1",
-        "GATA3",
-      ],
-    },
-    {
-      name: "Colorectal Cancer",
-      dataPoints: [
-        "KRAS",
-        "NRAS",
-        "BRAF",
-        "PIK3CA",
-        "TP53",
-        "APC",
-        "SMAD4",
-        "FBXW7",
-      ],
-    },
-    {
-      name: "Prostate Cancer",
-      dataPoints: [
-        "AR",
-        "TP53",
-        "PTEN",
-        "RB1",
-        "SPOP",
-        "FOXA1",
-        "NKX3-1",
-        "ATM",
-      ],
-    },
-    {
-      name: "Pancreatic Cancer",
-      dataPoints: [
-        "KRAS",
-        "TP53",
-        "CDKN2A",
-        "SMAD4",
-        "BRCA2",
-        "PALB2",
-        "ATM",
-        "ARID1A",
-      ],
-    },
-    {
-      name: "Melanoma",
-      dataPoints: [
-        "BRAF",
-        "NRAS",
-        "KIT",
-        "TP53",
-        "PTEN",
-        "CDKN2A",
-        "GNAQ",
-        "GNA11",
-      ],
-    },
-    {
-      name: "Ovarian Cancer",
-      dataPoints: [
-        "BRCA1",
-        "BRCA2",
-        "TP53",
-        "PIK3CA",
-        "KRAS",
-        "NRAS",
-        "BRAF",
-        "ERBB2",
-      ],
-    },
-    {
-      name: "Bladder Cancer",
-      dataPoints: [
-        "FGFR3",
-        "TP53",
-        "RB1",
-        "PIK3CA",
-        "ERBB2",
-        "KRAS",
-        "NRAS",
-        "ARID1A",
-      ],
-    },
-    {
-      name: "Liver Cancer",
-      dataPoints: [
-        "TP53",
-        "CTNNB1",
-        "AXIN1",
-        "TERT",
-        "ARID1A",
-        "RB1",
-        "PIK3CA",
-        "CDKN2A",
-      ],
-    },
-    {
-      name: "Esophageal Cancer",
-      dataPoints: [
-        "TP53",
-        "KRAS",
-        "PIK3CA",
-        "NRAS",
-        "APC",
-        "ERBB2",
-        "SMAD4",
-        "NOTCH1",
-      ],
-    },
-  ];
+    const interval = useRef<NodeJS.Timeout | null>(null);
+    const ref = useRef<HTMLDivElement>(null);
+    const isHovered = useMouseHover(ref);
+    const cancers = [
+        { name: "Lung Cancer", mutations: ["EGFR", "KRAS", "ALK", "ROS1"] },
+        { name: "Breast Cancer", mutations: ["BRCA1", "BRCA2", "HER2", "TP53"] },
+        { name: "Prostate Cancer", mutations: ["BRCA2", "HOXB13", "TP53", "PTEN"] },
+        { name: "Colon Cancer", mutations: ["APC", "KRAS", "TP53", "PIK3CA"] },
+        { name: "Bladder Cancer", mutations: ["FGFR3", "TP53", "RB1", "PIK3CA"] },
+        { name: "Melanoma", mutations: ["BRAF", "NRAS", "KIT", "GNAQ"] },
+        { name: "Leukemia", mutations: ["BCR-ABL1", "FLT3", "NPM1", "CEBPA"] },
+        { name: "Lymphoma", mutations: ["MYC", "BCL2", "BCL6", "EZH2"] },
+    ];
 
-  const [selectedCancer, setSelectedCancer] = useState(0);
-  const { name, dataPoints } = cancers[selectedCancer];
+    const large = useMediaQuery("(min-width: 1024px)");
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        if (interval.current) {
+            clearInterval(interval.current);
+        }
+        if (!isHovered) {
+            interval.current = setInterval(() => {
+                setIndex(
+                    (prev) => (prev + (large ? +1 : -1) + data.length * 2) % data.length
+                );
+            }, 1000);
+        }
+        return () => {
+            if (interval.current) {
+                clearInterval(interval.current);
+            }
+        };
+    }, [isHovered, large]);
 
-  useEffect(() => {
-    if (interval.current) {
-      clearInterval(interval.current);
-    }
-    if (!isHovered) {
-      interval.current = setInterval(() => {
-        setSelectedCancer((prev) => (prev + 1) % cancers.length);
-      }, 1000);
-    }
-    return () => {
-      if (interval.current) {
-        clearInterval(interval.current);
-      }
-    };
-  }, [isHovered]);
+    const cancer = cancers[index];
+    const near = [];
+    near.push(cancers[(index - 1 + cancers.length) % cancers.length]);
+    near.push(cancer);
+    near.push(cancers[(index + 1 + cancers.length) % cancers.length]);
+    
 
-  return (
-    <div
-      ref={ref}
-      className="flex gap-16 p-16 flex-col lg:flex-row items-stretch rounded-2xl bg-neutral-500 overflow-hidden"
-    >
-      <WheelCarousel
-        className="lg:justify-between lg:w-1/3 w-full"
-        selectedCancer={name}
-        setSelectedCancer={(cancer) => {
-          const index = cancers.findIndex((c) => c.name === cancer);
-          if (index !== -1) {
-            setSelectedCancer(index);
-          }
-        }}
-      />
-      <div className=" lg:w-2/3 w-full lg:self-stretch flex flex-col relative overflow-hidden">
-        <AnimatePresence mode="popLayout">
-          <motion.div
-            key={selectedCancer}
-            initial={{
-              transform: "translateY(100%)",
-            }}
-            animate={{
-              transform: "translateY(0%)",
-            }}
-            exit={{
-              transform: "translateY(-100%)",
-            }}
-            className=""
-          >
-            <AnimatedBeamMultipleOutputDemo className="" dataPoints={dataPoints} />
-          </motion.div>
-        </AnimatePresence>
-      </div>
-    </div>
-  );
+    return (
+        <div
+            ref={ref}
+            className="flex gap-16 p-16 flex-col lg:flex-row items-stretch rounded-2xl bg-neutral-500 overflow-hidden"
+        >
+            <WheelCarousel
+                className="lg:justify-between lg:w-1/3 w-full"
+                // cancers={near}
+                setSelectedCancer={(cancer) => {
+                    const index = cancers.findIndex((c) => c.name === cancer);
+                    if (index !== -1) {
+                        setSelectedCancer(index);
+                    }
+                }}
+            />
+            <div className=" lg:w-2/3 w-full lg:self-stretch flex flex-col relative overflow-hidden">
+                <AnimatePresence mode="popLayout">
+                    <motion.div
+                        key={selectedCancer}
+                        initial={{
+                            transform: "translateY(100%)",
+                        }}
+                        animate={{
+                            transform: "translateY(0%)",
+                        }}
+                        exit={{
+                            transform: "translateY(-100%)",
+                        }}
+                        className=""
+                    >
+                        <AnimatedBeamMultipleOutputDemo className="" dataPoints={cancer.mutations} />
+                    </motion.div>
+                </AnimatePresence>
+            </div>
+        </div>
+    );
 }
 
 export default MutationSection;
--- components/custom/wheelCarousel.tsx
+++ components/custom/wheelCarousel.tsx
@@ -7,87 +7,82 @@ import useMouseHover from "@/lib/useMouseHover";
 import { useMediaQuery } from "@/lib/useMediaQuery";
 
 type WheelCarouselProps = {
-  className?: string;
-  selectedCancer: string;
-  setSelectedCancer: (cancer: string) => void;
+    className?: string;
+    selectedCancer: string;
+    setSelectedCancer: (cancer: string) => void;
 };
 
 function WheelCarousel({
-  className,
-  selectedCancer,
-  setSelectedCancer,
+    className,
+    selectedCancer,
+    setSelectedCancer,
 }: WheelCarouselProps) {
-  const data = [
-    "Lung Cancer",
-    "Breast Cancer",
-    "Prostate Cancer",
-    "Colon Cancer",
-    "Bladder Cancer",
-    "Melanoma",
-    "Leukemia",
-    "Lymphoma",
-  ];
+    const data = [
+        "Lung Cancer",
+        "Breast Cancer",
+        "Prostate Cancer",
+        "Colon Cancer",
+        "Bladder Cancer",
+        "Melanoma",
+        "Leukemia",
+        "Lymphoma",
+    ];
 
-  const large = useMediaQuery("(min-width: 1024px)");
-  const [index, setIndex] = useState(0);
-  const interval = useRef<NodeJS.Timeout | null>(null);
-  const ref = useRef<HTMLDivElement>(null);
-  const isHovered = useMouseHover(ref);
+    const interval = useRef<NodeJS.Timeout | null>(null);
+    const ref = useRef<HTMLDivElement>(null);
+    
+    const large = useMediaQuery("(min-width: 1024px)");
+    const [index, setIndex] = useState(0);
+    useEffect(() => {
+        if (interval.current) {
+            clearInterval(interval.current);
+        }
+        if (!isHovered) {
+            interval.current = setInterval(() => {
+                setIndex(
+                    (prev) => (prev + (large ? +1 : -1) + data.length * 2) % data.length
+                );
+            }, 1000);
+        }
+        return () => {
+            if (interval.current) {
+                clearInterval(interval.current);
+            }
+        };
+    }, [isHovered, large]);
 
-  useEffect(() => {
-    if (interval.current) {
-      clearInterval(interval.current);
+      useEffect(() => {
+        setSelectedCancer(data[index]);
+      }, [index, data, setSelectedCancer]);
+
+    const show = 5;
+    const remaining = index + show - data.length;
+    let slice = data.slice(index, index + show);
+    if (remaining > 0) {
+        slice = slice.concat(data.slice(0, remaining));
     }
-    if (!isHovered) {
-      interval.current = setInterval(() => {
-        setIndex(
-          (prev) => (prev + (large ? +1 : -1) + data.length * 2) % data.length
-        );
-      }, 1000);
-    }
-    return () => {
-      if (interval.current) {
-        clearInterval(interval.current);
-      }
-    };
-  }, [isHovered, large]);
 
-  useEffect(() => {
-    setSelectedCancer(data[index]);
-  }, [index, data, setSelectedCancer]);
-
-  const show = 5;
-  const remaining = index + show - data.length;
-  let slice = data.slice(index, index + show);
-  if (remaining > 0) {
-    slice = slice.concat(data.slice(0, remaining));
-  }
-
-  return (
-    <div ref={ref} className={cn("flex lg:flex-col justify-center", className)}>
-      {slice.map((item, i) => (
-        <motion.div
-          className="lg:origin-left origin-top text-center text-3xl flex-1 flex items-center justify-center lg:text-[3.24vw] font-bold pl-2 w-max min-w-max"
-          initial={{
-            transform: `translate(${large ? "0" : "-100"}%, ${
-              !large ? "0" : "100"
-            }%) scale(${
-              1 - Math.abs(i - 2 + (large ? 1 : -1)) / 2
-            })`,
-            opacity: 1 - Math.abs(i - 2 + (large ? 1 : -1)) / 2,
-          }}
-          animate={{
-            transform: "translate(0%, 0%)",
-            opacity: 1 - Math.abs(i - 2) / 2,
-            transition: { duration: 0.5, ease: "easeInOut" },
-          }}
-          key={i + item}
-        >
-          {item}
-        </motion.div>
-      ))}
-    </div>
-  );
+    return (
+        <div ref={ref} className={cn("flex lg:flex-col justify-center", className)}>
+            {slice.map((item, i) => (
+                <motion.div
+                    className="lg:origin-left origin-top text-center text-3xl flex-1 flex items-center justify-center lg:text-[3.24vw] font-bold pl-2 w-max min-w-max"
+                    initial={{
+                        transform: `translate(${large ? "0" : "-100"}%, ${!large ? "0" : "100"}%) scale(${1 - Math.abs(i - 2 + (large ? 1 : -1)) / 2})`,
+                        opacity: 1 - Math.abs(i - 2 + (large ? 1 : -1)) / 2,
+                    }}
+                    animate={{
+                        transform: `translate(0%, 0%) scale(${1 - Math.abs(i - 2) / 2})`,
+                        opacity: 1 - Math.abs(i - 2) / 2,
+                        transition: { duration: 0.5, ease: "easeInOut" },
+                    }}
+                    key={i + item}
+                >
+                    {item}
+                </motion.div>
+            ))}
+        </div>
+    );
 }
 
 export default WheelCarousel;
