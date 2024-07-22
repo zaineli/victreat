ab1085fbbc6fce025c04560968f8eced3a7a62b5
Abbas-Askari <abbasaskari797@gmail.com>
2024-07-22 18:21:23 +0500

    Refactored: deleted useless public assets and moved function to utils

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,8 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+
+export function map(n: number, start1: number, stop1: number, start2: number, stop2: number) {
+  return (n - start1) / (stop1 - start1) * (stop2 - start2) + start2;
+}
